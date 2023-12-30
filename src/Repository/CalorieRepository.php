96e013d8bffa1ea5fbbba7f96601dd184d5f0256
Yokke <mdembelepro@gmail.com>
2023-12-30 15:34:13 +0100

    removing comments and adding new line in twig files

--- src/Repository/CalorieRepository.php
+++ src/Repository/CalorieRepository.php
@@ -21,28 +21,5 @@ class CalorieRepository extends ServiceEntityRepository
         parent::__construct($registry, Calorie::class);
     }
 
-//    /**
-//     * @return Calorie[] Returns an array of Calorie objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
 
-//    public function findOneBySomeField($value): ?Calorie
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
--- src/Repository/WeightRepository.php
+++ src/Repository/WeightRepository.php
@@ -21,28 +21,5 @@ class WeightRepository extends ServiceEntityRepository
         parent::__construct($registry, Weight::class);
     }
 
-//    /**
-//     * @return Weight[] Returns an array of Weight objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('w')
-//            ->andWhere('w.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('w.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
 
-//    public function findOneBySomeField($value): ?Weight
-//    {
-//        return $this->createQueryBuilder('w')
-//            ->andWhere('w.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
