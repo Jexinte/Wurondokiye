ffafefd9e6d109ef0c4fd8c847970670773b7fef
Yokke <mdembelepro@gmail.com>
2024-01-03 11:30:00 +0100

    update on securit.yaml users wasn't able to log in due to the provider that wasn't set and login function name on LoginController and code style

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -99,7 +99,7 @@ class User implements  UserInterface,PasswordAuthenticatedUserInterface
 
     public function getUserIdentifier(): string
     {
-        return $this->username;
+        return (string) $this->username;
     }
 
 
